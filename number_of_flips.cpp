693daca1ba4e29429f4e8e9506fea759e2b51545
Harish Gehlot <86105660+HashGehlot03@users.noreply.github.com>
2022-02-10 16:38:10 +0530

    Number of flips

---
+++ number_of_flips.cpp
@@ -0,0 +1,20 @@
+/*
+Problem Statement
+Ninja is learning the binary representation of the numbers. He wanted to practice the topic, so he picked a question. The problem statement says, two numbers, ‘A’ and ‘B’ are given. Find the number of bits of ‘B’ that should be flipped to convert it into ‘A’.Can you help Ninja to solve this problem?
+You are given two integers, ‘A’ and ‘B’.Find the number of bits of ‘B’ that should be flipped to convert it into ‘A’.
+*/
+
+int countSetBits(int n)
+{
+    int count = 0;
+    while (n > 0)
+    {
+        count++;
+        n &= (n-1);
+    }
+    return count;
+}
+int numberOfFlips(int a, int b)
+{
+    return countSetBits(a^b);
+}
