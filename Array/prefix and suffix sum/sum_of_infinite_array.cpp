247625be65a97f12e1b26e1c31a4e0ba196b4de9
Harish Gehlot <86105660+HashGehlot03@users.noreply.github.com>
2022-02-11 19:40:20 +0530

    Update sum_of_infinite_array.cpp

--- Array/prefix and suffix sum/sum_of_infinite_array.cpp
+++ Array/prefix and suffix sum/sum_of_infinite_array.cpp
@@ -6,3 +6,39 @@ For example, if the given array “A” is [1,2,3] then, infinite array “B” 
 Now you are given Q queries, each query consists of two integers “L“ and “R”. Your task is to find the sum of the subarray from index “L” to “R” (both inclusive) in the infinite array “B” for each query.
 
 */
+
+#include<iostream>
+using namespace std;
+int sum_of_infinite_array(int a[],int size,int L,int R)
+{
+	int b[R];
+	int sum = 0;
+	for(int i=0;i<=R;i++)
+	{
+		if(i<L) b[i] = a[i%size];
+		else
+		{
+			b[i] = a[i%size];
+			sum+=b[i];
+		}
+	}
+	return sum;
+}
+int main()
+{
+	int n,L,R;
+	cout<<"enter the size of array"<<endl;
+	cin>>n;
+	int a[n];
+	cout<<"enter the elements of array :"<<endl;
+	for(int i=0;i<n;i++)
+	{
+		cout<<"Enter element no. "<<i+1<<" ";
+		cin>>a[i];
+	}
+	cout<<"Enter the value of L and R"<<endl;
+	cin>>L;
+	cin>>R;
+	cout<<"the sum of subarray ranges between "<<L<<" and "<<R<<" is "<<sum_of_infinite_array(a,n,L,R);
+	return 0;
+}
